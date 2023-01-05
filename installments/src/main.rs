b12ce821a984f4f9ede49fd86db8c58fe6c83c73
Eduardo Daudt Flach <edudfla@gmail.com>
2023-01-05 16:43:24 +0100

    Split single function in sub functions.

--- installments/src/main.rs
+++ installments/src/main.rs
@@ -7,6 +7,12 @@ fn main() {
     let installment_str = args.get(2).expect("Installment value not provided");
     let interest_str = args.get(3).expect("Interest rate value not provided");
 
+    let (loan, installment, interest) = check_inputs(loan_str, installment_str, interest_str);
+    tabulate_installments(loan, installment, interest);
+ 
+}
+
+fn check_inputs(loan_str: &String, installment_str: &String, interest_str: &String) -> (f64, f64, f64) {
     let loan = f64::from_str(loan_str).expect("Loan seems not to be a number");
     let installment = f64::from_str(installment_str).expect("Installment seems not to be a number");
     let interest = f64::from_str(interest_str).expect("Interest seems not to be a number");
@@ -23,6 +29,10 @@ fn main() {
     if loan * interest / 100.0 >= installment {
         panic!("Debt unpayable (loan * interest / 100 >= installment)");
     }
+    (loan, installment, interest)
+}
+
+fn tabulate_installments(loan: f64, installment: f64, interest: f64) {
     let mut remaining: f64 = loan;
     println!("{}\t{:.2}\t{:.2}", 0, remaining, 0.0);
     let new_interest: f64 = 1.0 + interest / 100.0;
